9660749a79823b7a5d51b3fe132d3b7d390466b6
wsgk <29914462+wsgk@users.noreply.github.com>
2018-09-10 18:02:16 +0900

    typescript

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello from App Engine!");
 });
 
